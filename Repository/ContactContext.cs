9ef08f831f8b94a40c7bcf9078681a80c50d1a7b
Danilo-Oliveira-Silva <danilo.silva@betrybe.com>
2024-03-06 12:26:47 -0300

    fix: connection string

--- Repository/ContactContext.cs
+++ Repository/ContactContext.cs
@@ -15,8 +15,14 @@ public class ContactContext : DbContext, IContactContext
             //optionsBuilder.UseSqlServer(connectionString);
 
             //var connectionString = "Server=localhost;Database=ContactList;User Id=root;Password=123456;Port=3308";
+            var server = Environment.GetEnvironmentVariable("DBSERVER");
+            var database = Environment.GetEnvironmentVariable("DBDATABASE");
+            var user = Environment.GetEnvironmentVariable("DBUSER");
+            var password = Environment.GetEnvironmentVariable("DBPASSWORD");
+            var port = Environment.GetEnvironmentVariable("DBPORT");
 
-            var connectionString = "Server=viaduct.proxy.rlwy.net;Database=railway;User Id=root;Password=fcbBhGEfE2bGBHCbb2ebGbEcFBDfEBaE;Port=16280";
+            //var connectionString = "Server=viaduct.proxy.rlwy.net;Database=railway;User Id=root;Password=fcbBhGEfE2bGBHCbb2ebGbEcFBDfEBaE;Port=16280";
+            var connectionString = "Server="+server+";Database="+database+";User Id="+user+";Password="+password+";Port="+port;
             optionsBuilder.UseMySql(connectionString,ServerVersion.AutoDetect(connectionString),null);
         }
     }
